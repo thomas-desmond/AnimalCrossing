b62856f627c2830628ebef0e766e202296f8bc5c
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-05-19 20:39:13 -0700

    Not working Lambda experiments for verifying user token

--- frontend/animal-crossing/src/app/app.module.ts
+++ frontend/animal-crossing/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadImageModule } from './upload-image/upload-image/upload-image.module';
 import { NavMenuModule } from './nav-menu/nav-menu.module';
+import { UserVerificationComponent } from './user-verification/user-verification.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserVerificationComponent,
   ],
   imports: [
     BrowserModule,
