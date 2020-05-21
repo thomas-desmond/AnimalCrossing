1f24a80846b3f3a6b47515087661b784aeb1da5a
FreaknDeku <cruzjone@gmail.com>
2020-05-20 23:09:35 -0700

    cleanup; space between nav menu buttons

--- frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.component.ts
+++ frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadImageService } from 'src/app/service/upload-image.service';
-import { Directive, Output, Input, EventEmitter, HostBinding, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
