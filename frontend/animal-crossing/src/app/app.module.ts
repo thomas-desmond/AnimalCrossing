10fcccfae55a86aa00739aebac07ef8db4b343d6
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-05-20 19:46:46 -0700

    installed ngx-cookie-service package

--- frontend/animal-crossing/src/app/app.module.ts
+++ frontend/animal-crossing/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadImageModule } from './upload-image/upload-image/upload-image.module';
 import { NavMenuModule } from './nav-menu/nav-menu.module';
 import { UserVerificationComponent } from './user-verification/user-verification.component';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { UserVerificationComponent } from './user-verification/user-verification
     UploadImageModule,
     NavMenuModule
   ],
-  providers: [],
+  providers: [ CookieService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
