0bd3becfae00d7bd56cf8f855d9f7b7d2f1e3d9a
FreaknDeku <cruzjone@gmail.com>
2020-05-20 23:01:03 -0700

    WIP implementing upload image form

--- frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.component.ts
+++ frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadImageService } from 'src/app/service/upload-image.service';
+import { Directive, Output, Input, EventEmitter, HostBinding, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload-image',
@@ -8,16 +10,41 @@ import { UploadImageService } from 'src/app/service/upload-image.service';
 })
 export class UploadImageComponent implements OnInit {
 
-  constructor(private uploadImageService: UploadImageService) { }
+  public selectedFile = false;
+  public uploadImageForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private uploadImageService: UploadImageService
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.uploadImageForm = this.fb.group({
+      imageName: new FormControl('', [Validators.required]),
+    });
   }
 
   private fileToUpload: File;
 
   public handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
-    console.log(this.fileToUpload);
-    this.uploadImageService.uploadImage(this.fileToUpload);
+    console.log('this.fileToUpload', this.fileToUpload);
+    var oFReader = new FileReader();
+    //@ts-ignore
+    oFReader.readAsDataURL(document.getElementById("fileInput").files[0]);
+
+    oFReader.onload = function (oFREvent) {
+      //@ts-ignore
+      document.getElementById("uploadPreview").src = oFREvent.target.result;
+    };
+  }
+
+  public uploadImage() {
+    // this.uploadImageService.uploadImage(this.fileToUpload);
+
   }
 }
--- frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.module.ts
+++ frontend/animal-crossing/src/app/upload-image/upload-image/upload-image.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UploadImageComponent } from './upload-image.component';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UploadImageComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     UploadImageComponent
