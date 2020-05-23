142cc57a088259ee752d22305e77067b59915a39
FreaknDeku <cruzjone@gmail.com>
2020-05-22 18:16:50 -0700

    rename observable propertues; created userData Observable that gets and caches user data depending on token

---
+++ Angular/src/app/models/UserData.ts
@@ -0,0 +1,6 @@
+export class UserData {
+    public userName: string
+    public email: string
+    public email_verified: boolean
+  }
+  
--- Angular/src/services/authentication.service.ts
+++ Angular/src/services/authentication.service.ts
@@ -1,23 +1,53 @@
 import { Injectable } from "@angular/core";
 import { Constants } from 'src/app/constants/constants';
 import { CookieService } from 'ngx-cookie-service';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment'
+import { shareReplay } from 'rxjs/operators';
+import { UserData } from 'src/app/models/UserData';
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthenticationService {
 
-  public isAuthenticated = new Subject();
+  public isAuthenticated$ = new Subject();
+  private isAuthenticatedPreviousValue: boolean;
+  public userData$: Observable<UserData>
+
+  private getUserDataUrl = environment.apigatewayBaseUrl + '/get-user-data';
 
   constructor(
     private router: Router,
-    private cookieService: CookieService
-    ) {
+    private cookieService: CookieService,
+    private http: HttpClient
+  ) {
+    this.setupCookieSubject();
+    this.setupUserInfo()
+  }
+
+  private setupCookieSubject() {
     setInterval(() => {
-      this.isAuthenticated.next(this.cookieService.check(Constants.cookieTokenName));
+      const currentValue = this.cookieService.check(Constants.cookieTokenName);
+      if (this.isAuthenticatedPreviousValue !== currentValue) {
+        this.isAuthenticated$.next(this.cookieService.check(Constants.cookieTokenName));
+        this.isAuthenticatedPreviousValue = currentValue;
+      }
     }, 1000)
+  }
+
+  private setupUserInfo() {
+    if (this.cookieService.check(Constants.cookieTokenName)) {
+      const token = this.cookieService.get(Constants.cookieTokenName);
+      const requestOptions = {
+        headers: new HttpHeaders({
+          'Authorization': token
+        }),
+      };
+      this.userData$ = this.http.get<UserData>(this.getUserDataUrl, requestOptions).pipe(shareReplay());
+    }
   }
 
   public saveUserTokenCookie(tokenObj: any) {
@@ -26,6 +56,7 @@ export class AuthenticationService {
     )
 
     this.cookieService.set(Constants.cookieTokenName, tokenObj["id_token"], expirationDate);
+    this.setupUserInfo();
   }
 
   public logout() {
