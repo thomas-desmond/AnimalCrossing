652d2b3b1e44edf92154088a852cd9e565cac1da
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-06-05 13:39:57 -0700

    Created initial ImageMetadata model.

---
+++ Angular/src/app/models/ImageMetadata.ts
@@ -0,0 +1,9 @@
+export class ImageMetadata {
+    public imageGuid: string;
+    public title: string;
+    public designCode: string;
+    public description: string;
+    public uploadTime: Date;
+    public tags: string;
+    public user: string;
+}
